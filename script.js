aec8387d7907cfaaacc2f4aecff23fb3b03b39c2
Gleb Kotello <Kotello123@hotmail.com>
2025-03-06 23:30:38 +0300

    Тест-18

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
     } else {
         console.log("Все элементы найдены!");
 
-        const correctPassword = "Гойда";
+        const correctPassword = "7 марта";
 
         submitButton.addEventListener("click", () => {
             const userInput = passwordInput.value;
